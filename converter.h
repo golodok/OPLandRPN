1660bff7ae1dff47884c7dfe1f34e767f4c0ead6
Голодок Андрей <a.golodok@tcp.local>
2018-06-08 14:44:08 +0300

    Переделанный конвертер

---
+++ converter.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "stack.h"
+#include "map"
+#include "QList"
+#include <cctype>
+using stackTypePtr =  std::shared_ptr<Stack<std::string>>;
+class Converter
+{
+    std::string output;
+    std::map<char,std::string> map{{')',"0"},
+                                   {'(',"1"},
+                                   {'+',"2"},
+                                   {'-',"2"},
+                                   {'*',"3"},
+                                   {'/',"3"},
+                                   {'^',"4"},
+                                   {'%',"3"},
+                                  };
+
+public:
+
+    Converter();
+    void fromRPN(std::string);
+    bool isNumberFromRPN(QList<std::string> &QLInput);
+    void preparationInput(std::string & input, QList<std::string> &QLInput);
+    bool isZnackFromRPN(QList<std::string> &QLInput);
+    bool determinationOfZhackFromRPN(std::string & iPrioretet,QList<std::string> &QLInput);
+    void concatStackFromRPN(Stack<std::string> &stack2, Stack<std::string> &stack1, Stack<std::string> &m_stack, std::string & iPrioritet);
+    bool checkDataStackAndPuchStacksFromRPN(Stack<std::string> &stack2, Stack<std::string> & stack1,Stack<std::string> & m_stack);
+
+
+    void fromOPL(std::string);
+    bool isZnackFromOPL(QList<std::string> &QLInput);
+    void workWhistOperFromOPL(QList<std::string> &QLInput, Stack<std::string> & m_stack);
+    bool isCloseBracketQList(QList<std::string> &QLInput);
+
+    void printOutput(Stack<std::string>& m_stack , std::string & input);
+
+
+
+
+};
+
+
