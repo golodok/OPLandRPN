d156943f3f438ec29111ce511262df589089669e
Голодок Андрей <a.golodok@tcp.local>
2018-06-08 14:50:48 +0300

    Область видимости

--- converter.h
+++ converter.h
@@ -16,25 +16,24 @@ class Converter
                                    {'^',"4"},
                                    {'%',"3"},
                                   };
-
-public:
-
-    Converter();
-    void fromRPN(std::string);
     bool isNumberFromRPN(QList<std::string> &QLInput);
     void preparationInput(std::string & input, QList<std::string> &QLInput);
     bool isZnackFromRPN(QList<std::string> &QLInput);
     bool determinationOfZhackFromRPN(std::string & iPrioretet,QList<std::string> &QLInput);
     void concatStackFromRPN(Stack<std::string> &stack2, Stack<std::string> &stack1, Stack<std::string> &m_stack, std::string & iPrioritet);
     bool checkDataStackAndPuchStacksFromRPN(Stack<std::string> &stack2, Stack<std::string> & stack1,Stack<std::string> & m_stack);
-
-
-    void fromOPL(std::string);
     bool isZnackFromOPL(QList<std::string> &QLInput);
     void workWhistOperFromOPL(QList<std::string> &QLInput, Stack<std::string> & m_stack);
     bool isCloseBracketQList(QList<std::string> &QLInput);
 
     void printOutput(Stack<std::string>& m_stack , std::string & input);
+
+public:
+
+    Converter();
+    void fromRPN(std::string);
+    void fromOPL(std::string);
+
 
 
 
